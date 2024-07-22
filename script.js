cd41ae7f69404308a4908f2025a200c8dd1ce8f5
MUKESH-G-2003 <127624466+MUKESH-G-2003@users.noreply.github.com>
2024-07-22 10:32:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const { list } = require("tar");
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.className = "list";
+    li.textContent = taskInput.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'DELETE';
+    deleteButton.onclick = function() {
+        taskList.removeChild(li);
+    };
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+}
